b6c0d7a5eab0c302e0b27150a7fa5bccf474e72d
Tony <1366832341@qq.com>
2018-12-23 16:12:40 +0800

    ,,,

---
+++ app/src/main/java/com/example/sam/helloword/db/Province.java
@@ -0,0 +1,25 @@
+package com.example.sam.helloword.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getProvinceName(){
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName){
+        this.provinceName=provinceName;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
